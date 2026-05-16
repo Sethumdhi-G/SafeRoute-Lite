098a63a7e542f89b95cf465daf8aa77592489815
Sethumdhi-G <sathnara17@gmail.com>
2026-05-16 21:53:00 +0530

    Add imports for express, path, vite, and sqlite3
    Brining in tools to built SafeRoute Lite.

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
+import express from "express";
+import path from "path";
+import { createServer as createViteServer } from "vite";
+import Database from "better-sqlite3";
+import { fileURLToPAth } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+comst __dirname = path.dirname(__filename);
