bd759ee32823f94375c5e9fed15b20e5f903e6ff
Sethumdhi-G <sathnara17@gmail.com>
2026-05-16 22:42:50 +0530

    Configure server for development and production modes
    Including code to starting server and listing errors

--- server.ts
+++ server.ts
@@ -61,7 +61,32 @@ app.get("/api/notes", (req, res) => {
     }
   });
 
-  
+  //Using Vite only for development
+  if (process.env.NODE_ENV !== "production"){
+    const vite = await createViteServer({
+      server: { middlewareMode: true },
+      appType: "spa",
+    });
+    app.use(vite.middlewares);
+  }
+  else{
+    const distPath = path.join(process.cwd(), "dist");
+    app.use(express.static(distPath));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(distPath, "index.html"));
+    });
+  }
+
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+//if error during startup
+  startServer().catch((err) => {
+  console.error("Failed to start server:", err);
+});
+
       
     
   
